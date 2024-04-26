b8ab8e138e9113f93097e9a6d4e4297c3455b662
Kerren Ortlepp <kerren@entrostat.com>
2024-04-26 19:01:00 +0200

    Add the steps to get the flags needed

--- cmd/build.go
+++ cmd/build.go
@@ -14,21 +14,16 @@ been built. If it has been built, then skip the build and copy the tag,
 otherwise, build the new image and push it to the specified tag(s).`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("build called")
+		// First we need to check that the required flags are set.
+		// 1. Ensure that the directory flag is set and the directory exists
+		// 2. Ensure that the dockerfile-path flag is set and the file exists
+		// 3. Ensure that the image-name flag is set
+		// 4. Ensure that the version-file flag is set and the file exists
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(buildCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// buildCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// buildCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 
 	buildCmd.Flags().StringArrayP("watch-file", "w", []string{}, "Watch for changes on a specific file or files")
 	buildCmd.Flags().StringArrayP("watch-directory", "W", []string{}, "Watch for changes in a directory or directories")
@@ -37,7 +32,7 @@ func init() {
 	buildCmd.Flags().StringP("dockerfile-path", "f", "./Dockerfile", "(required) The path to the Dockerfile that should be used to build the image")
 	buildCmd.Flags().StringP("image-name", "i", "", "(required) The name of the image you are building")
 	buildCmd.Flags().BoolP("latest", "l", false, "Whether to push the latest tag with this image")
-	buildCmd.Flags().StringP("version-file", "v", "./package.json", "(required) The name of the JSON file that holds the version to be used in the build. This JSON file must have the 'version' key.")
+	buildCmd.Flags().StringP("version-file", "F", "./package.json", "(required) The name of the JSON file that holds the version to be used in the build. This JSON file must have the 'version' key.")
 	buildCmd.Flags().StringP("registry", "r", "", "The registry that should be used when pulling/pushing the image, Dockerhub is used by default")
 	buildCmd.Flags().StringArrayP("tag", "t", []string{}, "The tag or tags that should be attached to image")
 	buildCmd.Flags().StringP("docker-username", "u", "", "The username that should be used to authenticate the docker client. Ignore if you have already logged in.")
