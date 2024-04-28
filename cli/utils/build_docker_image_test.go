da86e4780f05aed4811316e6df792c92c55e45c0
Kerren Ortlepp <kerren@entrostat.com>
2024-04-28 12:48:54 +0200

    Add the basic test

--- cli/utils/build_docker_image_test.go
+++ cli/utils/build_docker_image_test.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"os"
 	"testing"
 )
 
@@ -8,5 +9,36 @@ func TestStandardBuildWhereHashExists(t *testing.T) {
 	// In this test, I'm going to test a build where the hash is the same.
 	// In that case, it should not trigger a build but should rather copy
 	// the tag from the existing image to the new image.
+	imageName := os.Getenv("TEST_IMAGE_NAME")
+	username := os.Getenv("DOCKER_USERNAME")
+	password := os.Getenv("DOCKER_PASSWORD")
+	if imageName == "" || username == "" || password == "" {
+		t.Skip("Skipping test because environment variables are not set")
+	}
+	testDirectory := "../../testing/e2e/base-test-image"
+	directory := testDirectory + "/build"
+	versionPath := testDirectory + "/version.json"
+
+	params := BuildDockerImageParams{
+		Directory:            directory,
+		DockerBuildFlags:     []string{},
+		DockerPassword:       password,
+		DockerUsername:       username,
+		DockerfilePath:       directory + "/Dockerfile",
+		IgnoreBuildDirectory: false,
+		ImageName:            imageName,
+		Latest:               false,
+		MainVersion:          false,
+		Registry:             "",
+		Tag:                  []string{"test-hash-exists"},
+		VersionFile:          versionPath,
+		WatchDirectory:       []string{},
+		WatchFile:            []string{},
+	}
+
+	err := BuildDockerImage(params)
+	if err != nil {
+		t.Errorf("Error building the docker image: %s", err)
+	}
 
 }
