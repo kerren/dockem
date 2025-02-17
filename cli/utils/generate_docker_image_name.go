4b0e90d1370447696aa3d0d6d03848bf32f14e00
Lynch Mwaniki <lsking16@gmail.com>
2025-02-17 20:49:12 +0200

    Add doc to generate docker image name

--- cli/utils/generate_docker_image_name.go
+++ cli/utils/generate_docker_image_name.go
@@ -2,6 +2,7 @@ package utils
 
 import "fmt"
 
+// GenerateDockerImageName Generates a docker image name with the format `org/imageName:hash` or `imageName:hash`
 func GenerateDockerImageName(registry string, imageName string, tag string) string {
 	if registry != "" {
 		return fmt.Sprintf("%s/%s:%s", registry, imageName, tag)
